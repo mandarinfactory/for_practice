c085e0fbe330534745a8eb18232b2a17a91f6381
mandarin <hoojunee@gmail.com>
2022-10-19 15:14:24 +0900

    basic_jQeury_01

---
+++ JS/jQuery_01.js
@@ -0,0 +1,8 @@
+//jQuery($) library, event, value//
+$(document).ready(function () {
+    $('button').click(function () {
+        
+    })
+})
+// $() = object를 찾는다, document = web-document(DOM)  ==> 비동기식 script 연동을 시킨다. (넘기고 webbrowser가 읽게 된다.), 비동기는 1번만 적으면 된다! //
+// event(click)내에서 적용되는 call-back function이 있다. ==> event가 이루어질때 실행되는 function이다. //
