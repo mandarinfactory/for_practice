60eed9173cd4a6e2d96f27011e86525876f298b6
mandarin <hoojunee@gmail.com>
2022-10-20 16:02:57 +0900

    jQuery_02

--- JS/05/jQuery.js
+++ JS/05/jQuery.js
@@ -1,16 +1,30 @@
 $(document).ready(function () { // call-back function! => 이 내부에서만 기록해야한다. //
-    function clickAddNum () {
-        var firstNum = $('.ex_one .input_one').val();
-        var secondNum = $('.ex_one .input_two').val();
+    function clickAddNum (numX) {
+        var firstNum = $(`.ex_${numX} .input_one`).val();
+        var secondNum = $(`.ex_${numX} .input_two`).val();
         var theFirstNum = parseInt(firstNum + secondNum);
-        var thirdNum = $('.ex_one .input_three').val();
-        var fourthNum = $('.ex_one .input_four').val();
+        var thirdNum = $(`.ex_${numX} .input_three`).val();
+        var fourthNum = $(`.ex_${numX} .input_four`).val();
         var theSecondNum = parseInt(thirdNum + fourthNum);
         var resultNum = theFirstNum + theSecondNum;
-        $('.ex_one p').text(resultNum);
+        $(`.ex_${numX} p`).text(resultNum);
         // .text&html로 p element의 값을 변경(result => resultNum)한다.//
     }
+
     $('.ex_one button').click(function () {
-        clickAddNum();
+        clickAddNum('one');
+    })
+    $('.ex_two button').click(function () {
+        clickAddNum('two');
+    })
+    $('.ex_three button').click(function () {
+        var submitGender = $('.ex_three input:checked').val()
+        $('.ex_three p').text(`you've choose ${submitGender}.`)
+        // pseudo class => :checked는 선택되어진 element를 말한다. //
+    })
+    $('.ex_four button').click(function () {
+        var selectVal = $('.ex_four select').val()
+        console.log(selectVal);
+
     })
 })
