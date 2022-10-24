7f6c2cd8e8e1912695aea3dd63a534b438ee6310
mandarin <hoojunee@gmail.com>
2022-10-24 16:11:29 +0900

    jQuery_07

--- JS/07.if,for statement/jQuery.js
+++ JS/07.if,for statement/jQuery.js
@@ -1,12 +1,17 @@
 // if, for statement //
+function searchNumber (numX, numY) {
+      var primeNumber = parseInt($(`.ex_${numX} input`).val())
+        if(primeNumber > numY) {
+            $(`.ex_${numX} .para_result`).text(true);
+        } else {
+            $(`.ex_${numX} .para_result`).text(false);
+        }// if() = condition(조건문) { true일경우의 result} else {false일경우의 result}
+}
 $(function () {
     $('.ex_one button').click(function () {
-        var firstNumber = parseInt($('.ex_one input').val())
-        if(firstNumber > 5) {
-            $('.ex_one .para_result').text(true);
-        } else {
-            $('.ex_one .para_result').text(false);
-        }
-        // if() = condition(조건문) { true일경우의 result} else {false일경우의 result}
+        searchNumber('one', 5);
     }) // click(event) call-back function
+    $('.ex_two button').click(function () {
+        searchNumber('two', 3);
+    })
 }) // ready function(call-back function => 항상 jQuery쓸때는 꼭 필요하다!)
