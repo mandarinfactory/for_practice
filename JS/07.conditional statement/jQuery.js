afb3f4ed78dc32c0da5199b2d1aa60eb41b39290
mandarin <hoojunee@gmail.com>
2022-10-24 18:12:37 +0900

    jQuery_07

--- JS/07.conditional statement/jQuery.js
+++ JS/07.conditional statement/jQuery.js
@@ -35,10 +35,30 @@ $('.ex_four button').click(function () {
     var objectType = $('.ex_four input:checked').val()   
     $('.ex_four .result').html(searchBloodType(objectType))
 })
+
+function primeEvenNumber(primeNumber) {
+    var thePrimeNumber = primeNumber
+    if (thePrimeNumber === 0) {
+    return '짝수입니다!'
+    } else if (thePrimeNumber === 1) {
+    return '홀수입니다!'
+    } else { 
+    return '숫자를 입력해주시기 바랍니다.'
+    }    
+}
+$('.ex_five button').click(function () {
+    var primeNumber = ((parseInt($('.ex_five input').val()))) % 2
+    $('.ex_five .result_five').text(primeEvenNumber(primeNumber))
+})
     }) // ready function(call-back function => 항상 jQuery쓸때는 꼭 필요하다!)
     /* conditional statement
     // syntax
     - if , if else(2분기), if else if else(다분기)
     // comparsion operator
     - >, <, >=, <= , ===, !==, &&, !(=false) 
+    */
+
+   /* assignment
+   // 입력한 숫자를 1로 나눈 나머지가 소수점만 남는다는것 = 1보다 작다.
+   // 숫자는 1로 나눈 나머지가 0이여야 한다.
     */
