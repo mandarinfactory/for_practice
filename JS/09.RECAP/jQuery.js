4573e6728a2e095c52bfd050c7ab8dc64c75d65e
mandarin <hoojunee@gmail.com>
2022-10-26 16:01:39 +0900

    jQuery_07

--- JS/09.RECAP/jQuery.js
+++ JS/09.RECAP/jQuery.js
@@ -13,7 +13,7 @@ $(function () {
 
      //computer가 1 ~ 3사이의 숫자를 random하게 선택한 후, string으로 변환
      var randomNum = Math.ceil(Math.random() * 3)// () * 3 = 0.00000001 ~ 2.99999999
-     if (randomNum === 1) randomNum = '가위'
+     if (randomNum === 1) randomNum = '가위' 
      else if (randomNum === 2) randomNum = '바위'
      else randomNum = '보'
     
@@ -38,4 +38,31 @@ $('.ex_one button').click(function () {
     if(!resultGame) return false // click시 잘못된 값을 넣었을때 Eventlistener도 false로 반환시키게 해준다.
     $('.ex_one .para_result').text(`사용자 : ${resultGame.objUser}, 컴퓨터 : ${resultGame.objComputer}, 결과 : ${resultGame.objResult}`)
     })//click
+
+
+
+    $('.ex_two button').click(function () {
+        var korScore = parseFloat($('.ex_two .korean').val())  
+        var engScore = parseFloat($('.ex_two .english').val())  
+        var mathScore = parseFloat($('.ex_two .math').val())  
+        var totalScore = korScore + engScore + mathScore;
+        var averageScore = (totalScore / 3).toFixed(2); // 소수점 2자리까지 끊어서 return해주는 function
+        var scoreToGrade
+        if(isNaN(averageScore)){
+            alert('숫자로 적어주시기 바랍니다!')
+            return false}
+        if (averageScore > 100 || averageScore < 0) {
+            alert('0-100점 사이만 입력해주시기 바랍니다!')
+            return false
+        } // 유효성검사(validation)는 else if로 묶기보단 따로 if로 적어준다.
+        /* 등급구하기 */
+        if(averageScore >= 90){
+            scoreToGrade = 'A'
+        } else if (averageScore < 90 && averageScore >= 80) {
+            scoreToGrade = 'B'
+        } else {
+            scoreToGrade = 'C'
+        }
+        console.log(scoreToGrade)
+    })
 })//call-back function!!
