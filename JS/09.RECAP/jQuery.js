f50eb5397a420fff52abbc33f82f95f8e4fb4fb7
mandarin <hoojunee@gmail.com>
2022-10-26 14:50:13 +0900

    jQuery_07

--- JS/09.RECAP/jQuery.js
+++ JS/09.RECAP/jQuery.js
@@ -12,11 +12,16 @@ $(function () {
      else theResult = '보' // 한줄일때는 {}를 제외할 수 있다.
 
      //computer가 1 ~ 3사이의 숫자를 random하게 선택한 후, string으로 변환
-     var randomNumber = Math.ceil(Math.random() * 3)// () * 3 = 0.00000001 ~ 2.99999999
-     if (randomNumber === 1) randomNumber = '가위'
-     else if (randomNumber === 2) randomNumber = '바위'
-     else randomNumber = '보'
-     console.log(randomNumber);
+     var randomNum = Math.ceil(Math.random() * 3)// () * 3 = 0.00000001 ~ 2.99999999
+     if (randomNum === 1) randomNum = '가위'
+     else if (randomNum === 2) randomNum = '바위'
+     else randomNum = '보'
+    
+    var resultGame 
+    if ((theResult === '가위' && randomNum === '보') || (theResult === '바위' && randomNum === '가위') || (theResult === '보' && randomNum === '바위')) resultGame = '유저 승'
+    else if ((theResult === '가위' && randomNum === '바위') || (theResult === '바위' && randomNum === '보') || (theResult === '보' && randomNum === '가위')) resultGame = '컴퓨터 승'
+    else resultGame = '비김'
+    $('.ex_one .para_result').text(`사용자 : ${theResult}, 컴퓨터 : ${randomNum}, 결과 : ${resultGame}`)
     // 0 ~ 2 (내림) : Math.floor()
     // 1 ~ 3 (올림) : Math.ceil() + 반올림은 Math.round()
     })//click
