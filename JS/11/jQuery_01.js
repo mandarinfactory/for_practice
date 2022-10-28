cc4b89d0050de3214704cbe66130b20a4f2a4dcd
mandarin <hoojunee@gmail.com>
2022-10-28 16:57:46 +0900

    jQuery_09

--- JS/11/jQuery_01.js
+++ JS/11/jQuery_01.js
@@ -19,7 +19,7 @@ $(function () {
     $('.ex2 .bg-none').click(function () {
       $('.ex2 .box').css({'background': 'none'})
     })//click
-  
+   // 추가적으로 내가 넣어야함!
     $('.ex2 .bg-color').click(function () {
       $('.ex2 .box').css({
         'background': 'yellow',
@@ -37,4 +37,16 @@ $(function () {
     $('.ex3 .toggle-class').click(function () {
         $('.ex3 .box').toggleClass('activeOne activeTwo')
     })
+
+    /* ex4 -------------------------------------------------------- */
+    $('.ex4 .slide_up').click(function () {
+        $('.ex4 .box').stop().slideUp('slide_up')
+    })
+    $('.ex4 .slide_down').click(function () {
+        $('.ex4 .box').stop().slideDown('slide_down')
+    })
+    $('.ex4 .slide_toggle').click(function () {
+        $('.ex4 .box').stop().slideToggle('slide_toggle')
+    })
   })//document.ready
+  /* animnation이 중첩될경우 .stop()을 사용해 animation이 누적되는걸 방지한다. */
