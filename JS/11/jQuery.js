ef57b8cc4dc4c5392e73e94f44f9e469fc58f13d
mandarin <hoojunee@gmail.com>
2022-10-28 14:35:36 +0900

    jQuery_09

--- JS/11/jQuery.js
+++ JS/11/jQuery.js
@@ -1,20 +1,27 @@
 $(function () {
-    function getScoreCal() {
+    function getScoreCal(numX) {
         var totalScore = 0; /* 만약 변수를 외부에서 선언하면 
         초기화가 되지 않고 누적되서 계산되게 된다. */
-        totalScore += parseInt($(`.div_one .kor`).val())
-        totalScore += parseInt($(`.div_one .eng`).val())
-        totalScore += parseInt($(`.div_one .math`).val())
+        totalScore += parseInt($(`.div_${numX} .kor`).val())
+        totalScore += parseInt($(`.div_${numX} .eng`).val())
+        totalScore += parseInt($(`.div_${numX} .math`).val())
         // totalScore이 증가한다.(증감연산자, +=를 사용)
-        var averageScore = (totalScore / 3).toFixed(1);
-        var Obj = {
-            objTotal : totalScore, // totalScore값을 objTotal키에 넣는다. => Obj객체에 있는 objTotal키이다.
-            objAverage : averageScore,
-        }//하나에 2개이상의 결과(return)를 가지는걸 객체(object)라고 한다.
-        return Obj
+        var averageScore = parseFloat((totalScore / 3).toFixed(1));
+        if(isNaN(averageScore)){
+            alert('정확한 점수를 적어주시기 바랍니다.');
+            return false
+        }
+        $(`.div_${numX} .result`).text(`총점:${totalScore}, 평균:${averageScore}`)
+        var obj = {
+            totalScore,
+            averageScore,
+        }
+        return obj;
     }//function
-    $('.div_one button').click(function () {
-        var result = getScoreCal();
-        $(`.div_one .result`).text(`총점:${result.objTotal}, 평균:${result.objAverage}`)
+    $('.ex_one button').click(function () {
+        var resultOne = getScoreCal('one');
+        var resultTwo = getScoreCal('two');
+        $('.ex_one>.result').text(`총점평균 : ${((resultOne.totalScore)+(resultTwo.totalScore))/2} 
+        총평균: ${((resultOne.averageScore)+(resultTwo.averageScore))/2}`)
     })//click
 })//call-back
