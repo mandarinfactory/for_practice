53eeed58ffb9fd6664695f8222157425d346b71b
mandarin <hoojunee@gmail.com>
2022-10-28 17:59:53 +0900

    jQuery_10

--- JS/12. this, data attribute/jQuery.js
+++ JS/12. this, data attribute/jQuery.js
@@ -1,5 +1,5 @@
 $(function () {
-
+ // ex_one //
   function getHandle (thisElement) { 
     var a = thisElement.attr('data-kor') /* this는 예약어! 이미 지정해져있다. 
     this = Eventlistener(event를 받는 element) => event자체를 줄여버린다.
@@ -7,14 +7,13 @@ $(function () {
     attr = value내에 있는 attribute값을 찾아준다. */
     console.log(a)
   }//getHandle
-
   $('.ex_one button').click(function () {
     getHandle($(this)); // $(this) = 'thisElement'라는 parameter로 들어가서 적용된다.
   })//click
-
+ // ex_two //
   var n = 1;
   $('.ex_two button').click(function () {
   n += parseInt($(this).val())
   $('.ex_two .result').text(n);
-  })
+  })//click
 })//call-back
