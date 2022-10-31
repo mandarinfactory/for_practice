d5a85907bd0c816b52a14d9b38488407e20b36fa
mandarin <hoojunee@gmail.com>
2022-10-31 15:22:27 +0900

    jQuery_12

--- JS/13. this, node search/jQuery.js
+++ JS/13. this, node search/jQuery.js
@@ -3,7 +3,7 @@ $(function (){
         var numY = numX.val() // 선택한 btn의 value값 =.attr('value')
         var color = numX.attr('data-color') // 선택한 btn의 임의의 attribute('data-color')
         $(`.ex_${numY} button`).css({'color':'inherit'}) // 후순위로 오면 모든 element가 없어져버린다.
-        numX.css({'color':color}) //.click된 button = this
+        numX.css({'color':color}) //.click된 button = this = numX => color는 'data-color'값을 가져온다.
     }//btnColor
     $('.ex_one button, .ex_two button').click(function () {
         btnColor($(this)) // = click한btn => 최대한 간소화 시켜야한다!
