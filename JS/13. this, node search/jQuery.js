507ec13f7e45337ef2e3b29da029b7f3317316e5
mandarin <hoojunee@gmail.com>
2022-10-31 15:16:43 +0900

    jQuery_12

--- JS/13. this, node search/jQuery.js
+++ JS/13. this, node search/jQuery.js
@@ -1,6 +1,11 @@
 $(function (){
-    $('.ex_one button').click(function () {
-        $('.ex_one button').css({'color':'inherit'}) // 후순위로 오면 모든 element가 없어져버린다.
-        $(this).css({'color':'firebrick'}) //.click된 button = this
+    function btnColor (numX){
+        var numY = numX.val() // 선택한 btn의 value값 =.attr('value')
+        var color = numX.attr('data-color') // 선택한 btn의 임의의 attribute('data-color')
+        $(`.ex_${numY} button`).css({'color':'inherit'}) // 후순위로 오면 모든 element가 없어져버린다.
+        numX.css({'color':color}) //.click된 button = this
+    }//btnColor
+    $('.ex_one button, .ex_two button').click(function () {
+        btnColor($(this)) // = click한btn => 최대한 간소화 시켜야한다!
     }) //click
 })//call-back
