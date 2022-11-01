1f5627355e2e798b4ea7f9834a62b0a98ac76513
mandarin <hoojunee@gmail.com>
2022-11-01 15:01:40 +0900

    jquery_18

--- JS/14/jquery.js
+++ JS/14/jquery.js
@@ -9,17 +9,38 @@ $(function () {
     $(`.ex_one button, .ex_two button`).click(function () {
         checkBgColor($(this));
     })//click
-    /* ex_three --------------------------------------------------------- */
+    /* ex_three, four --------------------------------------------------------- */
     function checkColor (numX) {
-        var bigNum = numX.parent().attr('data-num')
-        console.log(bigNum);
         var num = numX.text();
-        console.log(num);
+        var bigNum = numX.parent().attr('data-num')
         $(`.ex_${bigNum} .box${num}`).siblings('div').removeClass(`active1 active2 active3`) // 우선 다 꺼야함!
         $(`.ex_${bigNum} .box${num}`).toggleClass(`active${num}`)
-
+        
     }
     $('.ex_three button, .ex_four button').click(function () {
         checkColor($(this));
-    })
+    })//click
+    /* ex_five, six--------------------------------------------------------- */
+    function calculator (numX) {
+        var numOne = parseInt(numX.siblings('.num_one').val())
+        var numTwo = parseInt(numX.siblings('.num_two').val())
+        var operCal = numX.siblings('select').val()
+        if (operCal === '+') {
+            result = numOne + numTwo
+        } else if (operCal === '-') {
+            result = numOne - numTwo
+        } else if (operCal === '*') {
+            result = numOne * numTwo
+        } else {
+            result = (numOne / numTwo).toFixed(2)
+        }
+    }
+    $(`.ex_five button, .ex_six button`).click(function () {
+        /* var exNum = $(this).val() 
+        var numOne = parseInt($(`.ex_${exNum} .num_one`).val())
+        var numTwo = parseInt($(`.ex_${exNum} .num_two`).val())
+        var operCal = $(`.ex_${exNum} select`).val() */
+        calculator($(this))
+        $(this).siblings('.result').text(result) // node를 사용하면 따로 value값을 줄 필요가 없다!
+    })//click
 })//.document.ready
