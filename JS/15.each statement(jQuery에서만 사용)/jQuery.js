859f597c1b6ed60150cdf5d3f3fcceaf89beb121
mandarin <hoojunee@gmail.com>
2022-11-01 16:15:56 +0900

    jquery_18

---
+++ JS/15.each statement(jQuery에서만 사용)/jQuery.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.ex_one button').click(function () {
+        $('.ex_one button').each(function () {
+
+        }) /*  each = forEach(), 차레를 왔을때의 각각 event를 말한다. => 무조건 call-back함수를 써넣는다.
+        element의 갯수만큼 each event가 실행시켜준다. */
+    })//click
+})//call-back
