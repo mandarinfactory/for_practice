1c1cd73516f683091cc373464258d4f300982e9f
mandarin <hoojunee@gmail.com>
2022-11-04 18:07:22 +0900

    jQuery_22.11.04

--- JS/18.double for statement/jQuery.js
+++ JS/18.double for statement/jQuery.js
@@ -93,4 +93,51 @@ $(function () {
             $('.ex_six .result').append(`<br>`)//5개 별 하나왔으니까 한줄 내리기!
         }// for -i
     })// click
+    /* ex_six -------------------------------------------------------------------------- */
+    $('.ex_seven button').click(function () {
+        $('.ex_seven .result').empty();
+        for(var i = 1; i <= 3; i++ ){
+            for(var j = 1; j <= 3 - i; j++){
+                $('.ex_seven .result').append('☆')
+            }//for_01
+            for(var j = 1; j <= (2 * i - 1); j++){
+                $('.ex_seven .result').append('★')
+            }//for_02
+            for(var j = 1; j <= 3 - i; j++){
+                $('.ex_seven .result').append('☆')
+            }//for_03
+            $('.ex_seven .result').append(`<br>`)
+        }// for -i
+    })// click
+    /* ex_seven ------------------------------------------------------------------------ */
+    $('.ex_eight button').click(function () {
+        var point = 0;
+        for(var i = 1; i <= 3; i++){
+            var riddle = ($(`.ex_eight .q${i} input:checked`).val())
+            if (riddle === 'o') point ++;
+        }//for
+        $('.ex_eight .result').text(point)
+    })//click
+    /* ex_eight ------------------------------------------------------------------------ */
+    $('.ex_nine button').click(function () {
+        var checkCnt = 0; //문제를 맞춰서 증가할 값
+        for(var j = 1; j <= 3; j++){
+            var datumCnt = parseInt($(`.ex_nine .q${j}`).attr('data-cnt'))//data-cnt값
+        }//for-j // 한문제에 돌리는 수 구하기
+        /* 여기까지가 1문제!*/
+        var point = 0;
+        for(var j = 1; j <= 3; j++){
+            var riddle = $(`.ex_nine .q${j} input:nth-child(${j})`).val()
+            var isCheck = $(`.ex_nine .q${j} input:nth-child(${j})`).prop('checked')// checkbox는 체크한 값이 1개 이상이라서 따로 prop으로 checked를 써야한다.
+            console.log(isCheck)
+            if(isCheck && riddle === 'o' /* 가져온값(isCheck)이고 그값이 답일때 */) {
+                point ++
+            } //if-correct
+            if(isCheck && riddle === undefined /* 가져온값(isCheck)이고 그값이 답이 아닐때 */){
+                point --
+                return false
+            } // if-incorrect
+            console.log(point)
+        }//for-j // data-cnt의 값 구하기      
+    })// click
 }) // call-back function
