ff171a6b75a9905588217e2a8334e97d2ff816b2
mandarin <hoojunee@gmail.com>
2022-11-07 13:11:04 +0900

    markup_22.11.07

---
+++ HTML/19.CSS(transform)/another/jQuery.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.door_btn').click(function () {
+        $('.door_container').toggleClass('active')
+    })//click
+    $('.cube_btns button').click(function (){
+        var X = $(this).attr('data-x')
+        var Y = $(this).attr('data-y')
+        $('.cube').css({'transform':`rotateX(${X}deg) rotateY(${Y}deg)`})
+    })//click
+})//call-back function
