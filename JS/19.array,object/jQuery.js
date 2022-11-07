728231c19c5c158102fc3a8e53ff966f1f473837
mandarin <hoojunee@gmail.com>
2022-11-07 18:14:32 +0900

    jquery_22.11.07

--- JS/19.array,object/jQuery.js
+++ JS/19.array,object/jQuery.js
@@ -11,12 +11,10 @@ $(function () {
     특정 index로부터 시작해서[1] 2가지 값을 지운다.[2] // 2, 3
     지우고 나서 'A', 'B'를 추가한다. // [1, A, B, 4, 5, 6]
     */
-   console.log(a.length); // 6
    /* practice -------------------------------------------------- */
    var b = ['김', '이', '박', '최'] // click-event에서 만들면 새로고침되서 array내 값이 지워지지 않는다!
    var c = 1
    function clickBtn (d) {
-    var d = $(this)
     if(b.length === 0){
         alert('끝났습니다.')
         return false
@@ -29,34 +27,92 @@ $(function () {
        if(b.length === 4) $(`.ex_${d} .result`).empty()
        $(`.ex_${d} .result`).append(`${Surname}은 ${5-b.length}등 입니다.`)
        b.splice(randomNum,1); // 출력된값을 지운다. randomNum로부터 시작해서 1가지 값을 지운다. ~ 0떼까지!
-       }//for-i
+       }//for-i => 총4번 반복!
    }//clickBtn function
    $('.ex_one button, .ex_two button').click(function () {  
     clickBtn($(this))
    })//click
    /* ex_one, two ----------------------------------------------- */
-var arrOne = [
-        ['김철수','25','남'], // arrOne[i번째]value => 0번째 // arrOne[i][j]번째 => 0번째줄의 0,1,2값들
-        ['박영희','38','여'],
-        ['최민영','41','여'],
-        ['이민국','19','남']
+   var arrOne = [
+       ['김철수','25','남'], // arrOne[i번째]value => 0번째 // arrOne[i][j]번째 => 0번째줄의 0,1,2값들
+       ['박영희','38','여'],
+       ['최민영','41','여'],
+       ['이민국','19','남']
     ]//arrOne, []안에 []있는 array를 multi-dimension array라고 한다.
-$('.ex_three button').click(function () {
-    $('.ex_three .result').empty()
-    $('.ex_three .result').html(`
-    <table>
+    $('.ex_three button').click(function () {
+        $('.ex_three .result').empty()
+        $('.ex_three .result').html(`
+        <table>
         <tr>
-            <th>이름</th>
-            <th>나이</th>
-            <th>성별</th>
+        <th>이름</th>
+        <th>나이</th>
+        <th>성별</th>
         </tr>
     </table>
     `)//html
     for(var i = 0; i <= arrOne.length; i++){
-    $('.ex_three table').append('<tr></tr>') 
+        $('.ex_three table').append('<tr></tr>') 
         for(var j = 0; j < arrOne[i].length; j++) {
             $('.ex_three tr:last-child').append(`<td>${arrOne[i][j]}</td>`)
-            }//for-j, j는 배열의 각각 칸에 들어갈 값!
+         }//for-j, j는 배열의 각각 칸에 들어갈 값!
         }//for-i, i는 배열의 줄!(총4줄)
+    })//click
+/* ex_three --------------------------------------------------- */
+var totalUserArr = [
+    []
+]//arrTwo
+$('.ex_four .add').click(function () {
+    var userArr = [] // ['','','']
+    userArr.push($('.ex_four .name').val()) // ['']
+    userArr.push($('.ex_four .age').val()) // ['','']
+    userArr.push($('.ex_four .gender').val()) // ['','','']
+    totalUserArr.push(userArr)
+    /* 
+    totalUserArr = [
+        ['','',''],
+        ['','',''],
+        .......
+    ] */
+    $('.ex_four .print').click(function () {
+        $('.ex_four .result').empty()
+        $('.ex_four .result').html(`
+        <table>
+        <tr>
+        <th>이름</th>
+        <th>나이</th>
+        <th>성별</th>
+        </tr>
+        </table>`)//html
+        for(var i = 0; i <= totalUserArr.length; i++) {
+            $('.ex_four table').append(`<tr></tr>`)
+            for(var j = 0; j < totalUserArr[i].length; j++) {
+                $('.ex_four tr:last-child').append(`<td>${totalUserArr[i][j]}</td>`)
+            }//for - j
+        }//for - i
+    })//click(.print)
+})//click(.add)
+/* ex_four ---------------------------------------------------- */
+var checkAnNum = [
+    [2, 3], //chk2, chk3 < q1
+    [1] //chk1 < q2
+]// checkAnNum (for - i) => 정답의 갯수는 j.length
+    $('.ex_five button').click(function () {
+        var point = 0;
+        var checkCnt = 0;
+        if ($(`.ex_five .q1 .chk${checkAnNum[1][0]}`).prop('checked')) {
+            checkCnt++
+        }
+        if ($(`.ex_five .q1 .chk${checkAnNum[1][1]}`).prop('checked')) {
+            checkCnt++
+        }
+        console.log(checkCnt)
+        if ($(`.ex_five .q2 .chk${checkAnNum[1][0]}`).prop('checked')) {
+            checkCnt++
+        }
+        console.log(checkCnt)
+    if(checkCnt === checkAnNum[1].length){
+        point++
+    }    
+    console.log(point)
     })//click
 })//call-back function
