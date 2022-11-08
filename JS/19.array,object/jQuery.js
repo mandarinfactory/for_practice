3395b395f9c006f55b12bb545d14bf7e4017d691
mandarin <hoojunee@gmail.com>
2022-11-08 10:15:47 +0900

    markup_22.11.08

--- JS/19.array,object/jQuery.js
+++ JS/19.array,object/jQuery.js
@@ -97,27 +97,18 @@ var checkAnNum = [
     [1] //chk1 < q2
 ]// checkAnNum (for - i) => 정답의 갯수는 j.length
 $('.ex_five button').click(function () {
-        var point = 0;
-        var checkCnt = 0;
-        if ($(`.ex_five .q1 .chk${checkAnNum[0][0]}`).prop('checked')) {
-            checkCnt++
-        }
-        console.log(checkAnNum[0][0])
-        if ($(`.ex_five .q1 .chk${checkAnNum[0][1]}`).prop('checked')) {
-            checkCnt++
-        }
-        console.log(checkAnNum[0][1])
-        if ($(`.ex_five .q2 .chk${checkAnNum[1][0]}`).prop('checked')) {
-            checkCnt++
-        }
-        console.log(checkAnNum[1][0])
-        console.log(checkCnt)
-    if(checkCnt === checkAnNum[0].length){
-        point++
-    }    
-    if(checkCnt === checkAnNum[1].length){
-        point++
-    }    
-    console.log(point)
+    var point = 0;
+    var checkCnt = 0;
+    for(var k = 1; k <= 2; k++){
+        for(var i = 0; i < checkAnNum.length; i++){
+            for(var j = 0; j < checkAnNum[i].length; j++){
+                if ($(`.ex_five .q${k} .chk${checkAnNum[i][j]}`).prop('checked')) {
+                    checkCnt++
+                        }//if 
+                    }//for-j
+            if(checkCnt === checkAnNum[i].length) point++ //if
+        }//for-i
+        $('.ex_five .result').text(point)
+    }//for-k
     })//click
 })//call-back function
