1099aaeed14f8aa2892a1c49a9c6530fe9ea29d5
mandarin <hoojunee@gmail.com>
2022-11-23 17:27:55 +0900

    jquery_22.11.23

--- JS/29.scroll-motion,each/jQuery.js
+++ JS/29.scroll-motion,each/jQuery.js
@@ -8,8 +8,12 @@ $(function () {
             offT = $(this).offset().top // this = '.section1 li:nth-child(n)'
             if(scrY >= offT - winH * 0.9 && scrY <= offT - winH * 0.1){
                 $(this).addClass('active')
+                $(this).children('div').children('em').addClass('active')
+                $(this).children('div').children('p').addClass('active')
             } else {
                 $(this).removeClass('active')
+                $(this).children('div').children('em').removeClass('active')
+                $(this).children('div').children('p').removeClass('active')
             }
         })
     }//function
