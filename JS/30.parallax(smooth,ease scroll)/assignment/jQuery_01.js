de926a9015e506b8a80d5fc39e62d97ce913c5b2
mandarin <hoojunee@gmail.com>
2022-11-25 15:57:11 +0900

    jquery_22.11.25

--- JS/30.parallax(smooth,ease scroll)/assignment/jQuery_01.js
+++ JS/30.parallax(smooth,ease scroll)/assignment/jQuery_01.js
@@ -1,18 +1,30 @@
 $(function () {
-    var t
-    var h
-    var change
-    function exParallax (){
-        change = 100 + (scrY - (t)) * 1
-        t = $('section div div').offset().top
-        h = $('section div div').innerHeight();
-        change =  0 + (scrY - (t - winH * 0.5 + h * 0.5)) * 0.1 
-        $('section div div').css({'transform':`scale(${change/50}) translateY(${change}px)`})
-    }//exParallax
-    exParallax();
-    $(window).scroll(function () {
-        exParallax();
-    }).resize(function () {
-        exParallax();
-    })//window-event
+    var offsetTop
+    var boxHeight
+    var meta
+
+ function motion () {
+    $('li').each(function (){
+        offsetTop = $(this).offset().top;  
+        boxHeight = $(this).innerHeight();
+        meta = 0 + Math.abs( scrY - (offsetTop - winH * 0.5 + boxHeight * 0.5)) * 0.1
+        if(meta > 50)meta = 50;
+        $(this).children('img').css({'border-radius':`${meta}%`})
+        meta = 1 + Math.abs( scrY - (offsetTop - winH * 0.5 + boxHeight * 0.5)) * - 0.0001
+        if(meta < 0)meta = 0;
+        $(this).children('img').css({'transform':`scale(${meta})`})
+    })
+ }//motion
+ motion();
+ $(window).scroll(function () {
+    motion();
+}).resize(function () {
+    motion();
+ })//window-event
+/* ex_one ------------------------------------------------------------------------- */
+
+
+
+
+
 })//document.ready
