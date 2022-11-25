8e0003a74c503eb4fb4a41b8ebfc8575cd7894ba
mandarin <hoojunee@gmail.com>
2022-11-25 16:44:21 +0900

    jquery_22.11.25

--- JS/30.parallax(smooth,ease scroll)/assignment/num_01/jQuery.js
+++ JS/30.parallax(smooth,ease scroll)/assignment/num_01/jQuery.js
@@ -8,14 +8,18 @@ $(function () {
             elH = $(this).innerHeight();
             meta = 0 + (scrY - (offT - winH * 0.5 + elH * 0.5)) * 0.1
             $(this).find('img').css({'transform':`scale(1.5) translateY(${meta}px)`})
-            meta = 1 + Math.abs(scrY - (offT - winH * 0.5 + elH * 0.5)) * - 0.0001
+            meta = 1 + Math.abs(scrY - (offT - winH * 0.5 + elH * 0.5)) * - 0.0005
             if(meta < 0) meta = 0;
             $(this).find('.inner').css({'transform':`scale(${meta})`})
+            /* parallax ------------------------------------------------------------ */
+            if (scrY >= offT  - winH * 0.9){
+                $(this).addClass('active')
+            } else {
+                $(this).removeClass('active')
+            }//if-else
+            /* scroll --------------------------------------------------------------- */    
         })//each
     }//function_move
-
-
-
     move();
     $(window).scroll(function () {
         move();
