0b0a711ebd1c937086a6760c7fccf6bf7dd8131b
mandarin <hoojunee@gmail.com>
2022-11-28 17:59:12 +0900

    vanila_22.11.28

--- JS/vanila/02.arrow function(화살표함수)/apps.js
+++ JS/vanila/02.arrow function(화살표함수)/apps.js
@@ -1,6 +1,17 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    window.addEventListener('load',()=>{
+document.addEventListener('DOMContentLoaded',() => {
+    window.addEventListener('load',() => {
+        function foo () {
 
+        }//1번째 방법 => hoisting이 발생한다.
+
+        foo = function () {
+
+        }//2번쩨 방법 => 1번째 방법과 다를게 없다.(전역변수인 window.가 생략된거와 같다.)
+        
+        const foo = () => {
+
+        }//3번째 방법 
+        foo(); // 항상 선언 후 호출임을 명시하자!
     })//load, tag에 있는 data까지 다 보려고 할때
     /*  
     load(얘는 DOM~내에 있어도 가능하다. load가 항상 나중에 발생한다. 단, 반대는 안됨!) 
