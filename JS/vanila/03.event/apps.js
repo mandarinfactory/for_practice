ca474dd033f1b626653870c159cd8f5219b55ed7
mandarin <hoojunee@gmail.com>
2022-11-29 09:46:47 +0900

    github_22.11.29

--- JS/vanila/03.event/apps.js
+++ JS/vanila/03.event/apps.js
@@ -1,7 +1,9 @@
 var a = 1;
 var b = 1;
 var sum;
+function sumNum(a,b){
+    return a + b;
+}
 $('button').click(function () {
-    sum = a + b;
-    console.log(sum);
+    console.log(sumNum(a, b));
 })
