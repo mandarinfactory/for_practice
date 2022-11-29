5ff83eeea7efd78d3425cc99c7fd8a4cebb7fa08
mandarin <hoojunee@gmail.com>
2022-11-29 17:46:56 +0900

    vanila_22.11.29

--- JS/vanila/04.CSS제어,HTML속성/apps.js
+++ JS/vanila/04.CSS제어,HTML속성/apps.js
@@ -6,3 +6,39 @@
         })//click-event 
     })//querySelectorAll
 }/* 이미 defer로 비연동으로 되어있기 때문에 따로 document필요없다! */
+/* ex_one --------------------------------------------------------------------- */
+{
+    document.querySelectorAll('.ex_two button').forEach( v => {
+        v.addEventListener('click', e => {
+            e.target.classList.toggle('active')
+        })//click-event 
+    })//querySelectorAll
+}
+/* ex_two --------------------------------------------------------------------- */
+{
+    document.querySelectorAll('.ex_three button').forEach( v => {
+        v.addEventListener('click', e => {
+            e.target.innerHTML = e.target.getAttribute('value')//'value'속성을 get한다.
+        })//click-event 
+    })//querySelectorAll
+}
+/* ex_three --------------------------------------------------------------------- */
+{
+    document.querySelectorAll('.ex_four button').forEach ( v => {
+        v.addEventListener('click', e => {
+            let a = e.target.innerHTML
+            let b = e.target.getAttribute('value')
+            e.target.innerHTML = b;
+            e.target.setAttribute('value', a)
+        })//click-event 
+    })//querySelectorAll
+}
+/* ex_four --------------------------------------------------------------------- */
+{
+    document.querySelectorAll('.ex_five button').forEach ( v => {
+        v.addEventListener('click', e => {
+            e.target.classList.toggle('active')
+        })//click-event 
+    })//querySelectorAll
+}
+/* ex_five --------------------------------------------------------------------- */
