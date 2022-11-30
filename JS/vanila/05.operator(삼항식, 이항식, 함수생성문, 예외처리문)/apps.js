8f9f7a65e2718d551369d602df1fbd84b8ad557d
mandarin <hoojunee@gmail.com>
2022-11-30 16:31:52 +0900

    vanila_22.11.30

--- JS/vanila/05.operator(삼항식, 이항식, 함수생성문, 예외처리문)/apps.js
+++ JS/vanila/05.operator(삼항식, 이항식, 함수생성문, 예외처리문)/apps.js
@@ -1,7 +1,7 @@
 {
     let a = 1;
     document.querySelector('.ex_one button').addEventListener('click', e => {
-        a =(a === 1)? 2 : 1;
+        a = (a === 1)? 2 : 1;
         console.log(a);
     })//click-event
     /* 3항연산자 ---------------------------------------------------------------- */
@@ -9,7 +9,8 @@
 {
     let a = 1;
     document.querySelector('.ex_two button').addEventListener('click', e => {
-        a =(a === 1)&&2;
+        a = (a === 1)||2; // a === 1이면 true, !== 이면 2를 return
+        a = (a === 1)&&2; // a === 1이면 2, !== 이면 false를 return
         console.log(a);
     })//click-event
     /* 2항연산자 ---------------------------------------------------------------- */
