073a385f300fd8cabea6aefaf06378fe5b1a03f4
mandarin <hoojunee@gmail.com>
2022-11-30 17:21:17 +0900

    vanila_22.11.30

--- JS/vanila/05.operator(삼항식, 이항식, 함수생성문, 예외처리문)/apps.js
+++ JS/vanila/05.operator(삼항식, 이항식, 함수생성문, 예외처리문)/apps.js
@@ -16,12 +16,21 @@
     /* 2항연산자 ---------------------------------------------------------------- */
 }
 {
-    n = 1;
-    const foo = new Function('a','b',`return(a + b)`)
+    let str = `1 + 2 * 3`
+    const foo = new Function (`str`,`return(str)`)
+    /* new Function(`str`,`return(1 + 2 * 3)`) 꼴로 나온다고 생각하면 된다. */
 
-    
+
     document.querySelector('.ex_three button').addEventListener('click', e => {
-       console.log(foo(1, 2));
+       console.log(foo(str));
     })//click-event
     /* 함수생성문 ---------------------------------------------------------------- */
+}
+{   
+    document.querySelector('.ex_four button').addEventListener('click', e => {
+        window.str = document.querySelector('.ex_four input').value /* input안에 있는 value! */
+        let calculator = new Function (`return ${str}`)
+        console.log(calculator())
+    })//click-event
+    /* 함수생성문_assignment ----------------------------------------------------- */
 }
