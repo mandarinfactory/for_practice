be099c540b9dc50084376d45d9b05c0809bb9102
mandarin <hoojunee@gmail.com>
2022-12-02 12:53:35 +0900

    markup_22.12.02

--- HTML/21.CSS(SVG)/jQuery.js
+++ HTML/21.CSS(SVG)/jQuery.js
@@ -1,6 +1,14 @@
 $(function () {
-      var morph = new TimelineMax({paused:true}); 
+      var morph = new TimelineMax({paused:false,repeat:-1}); 
 
       morph.to(".ex_four .path1",1,{morphSVG:".ex_four .path2",ease:Linear.easeNone})
-      .to(".ex_four .path2", 1,{morphSVG:".ex_four .path1_copy",ease:Linear.easeNone});
+      .to(".ex_four .path1", 1,{morphSVG:".ex_four .path1_copy",ease:Linear.easeNone});
+
+    /* ex_four ---------------------------------------------------- */
+    $('.ex_five .btn1').click(function () {
+        TweenMax.to(".ex_five .path1",1,{morphSVG:".ex_five .path1_copy",fill:"yellow",ease:Linear.easeNone})
+    })
+    $('.ex_five .btn2').click(function () {
+      TweenMax.to(".ex_five .path1",1,{morphSVG:".ex_five .path2",fill:"yellow",ease:Linear.easeNone})
+    })
 })
