aa6269f092f8b26c9afdb7050e8041a5e36ceae8
mandarin <hoojunee@gmail.com>
2022-12-02 16:55:51 +0900

    vanila_22.12.02

--- JS/vanila/07.date객체/apps.js
+++ JS/vanila/07.date객체/apps.js
@@ -1,20 +1,14 @@
 {
-    let halfHour
-    if (new Date().getHours() > 12){
+    /*     if (new Date().getHours() > 12){
         halfHour = (new Date().getHours() - 12 )
-    } else ( halfHour = new Date().getHours())
+    } else ( halfHour = new Date().getHours()) */
+    let dayNight = (new Date().getHours() > 12)?'PM':'AM'
+    let halfHour = (new Date().getHours() > 12)?new Date().getHours() - 12:new Date().getHours()
     if (halfHour < 10){
         halfHour = '0' + halfHour
     }
-    let min = new Date().getMinutes()
-    let sec = new Date().getSeconds()
-    if (min < 10){
-        min = '0' + min
-    }
-    if (sec < 10){
-        sec = '0' + sec
-    }
-
+    let min = (new Date().getMinutes() < 10)?new Date().getMinutes() = '0' + new Date().getMinutes():new Date().getMinutes()
+    let sec = (new Date().getSeconds() < 10)?new Date().getSeconds() = '0' + new Date().getSeconds():new Date().getSeconds()
     let year = new Date().getFullYear();
     let month = new Date().getMonth();
     let date = new Date().getDate();
@@ -24,7 +18,19 @@
     let monthArr = ['Janurary', 'Febrary','March','April','May','June','July','August','September','October','November','December']
     let dayArr = ['일','월','화','수','목','금','토']
     let str = ''
-    str += `${year}년도 ${monthArr[month]} ${date}일 ${dayArr[day]}요일 ${halfHour}시 ${min}분 ${sec}초`
-    document.querySelector('.ex_one .output').innerText = str
+    str += `${year}년도 ${monthArr[month]} ${date}일 ${dayArr[day]}요일 ${dayNight} ${halfHour}시 ${min}분 ${sec}초`
 
+
+
+
+
+    let foo = () => {
+        
+    }
+    document.querySelector('.ex_one .output').innerText = str
+    document.querySelectorAll('.ex_one button').forEach(v => {        
+        v.addEventListener('click', e => {
+            document.querySelector('.ex_one .btns').classList.toggle('active')
+        })
+    })
 }
