94062407aeba283808863111d37600b373d70e45
mandarin <hoojunee@gmail.com>
2022-12-02 17:08:35 +0900

    vanila_22.12.02

--- JS/vanila/07.date객체/apps.js
+++ JS/vanila/07.date객체/apps.js
@@ -7,30 +7,36 @@
     if (halfHour < 10){
         halfHour = '0' + halfHour
     }
-    let min = (new Date().getMinutes() < 10)?new Date().getMinutes() = '0' + new Date().getMinutes():new Date().getMinutes()
-    let sec = (new Date().getSeconds() < 10)?new Date().getSeconds() = '0' + new Date().getSeconds():new Date().getSeconds()
+    let min = new Date().getMinutes();
+    let sec = new Date().getSeconds();
     let year = new Date().getFullYear();
     let month = new Date().getMonth();
     let date = new Date().getDate();
     let day = new Date().getDay();
 
-
     let monthArr = ['Janurary', 'Febrary','March','April','May','June','July','August','September','October','November','December']
     let dayArr = ['일','월','화','수','목','금','토']
     let str = ''
-    str += `${year}년도 ${monthArr[month]} ${date}일 ${dayArr[day]}요일 ${dayNight} ${halfHour}시 ${min}분 ${sec}초`
-
-
-
-
 
     let foo = () => {
-        
+        str += `${year}년도 ${monthArr[month]} ${date}일 ${dayArr[day]}요일 ${dayNight} ${halfHour}시 ${min}분 ${sec}초`
+        document.querySelector('.ex_one .output').innerText = str
     }
-    document.querySelector('.ex_one .output').innerText = str
+    let intervalID = setInterval(() => {
+        foo()
+    },1000)
     document.querySelectorAll('.ex_one button').forEach(v => {        
         v.addEventListener('click', e => {
             document.querySelector('.ex_one .btns').classList.toggle('active')
+        })
+        document.querySelector('.ex_one .btns .stop').addEventListener('click', e => {
+            clearInterval(intervalID);
+        })
+        document.querySelector('.ex_one .btns .play').addEventListener('click', e => {
+            clearInterval(intervalID);
+            intervalID = setInterval(() => {
+                foo()
+            },1000)
         })
     })
 }
