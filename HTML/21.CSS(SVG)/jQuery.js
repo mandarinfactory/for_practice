d9b530e8f639b8a49fcde2cacc6ecfecd9d83135
mandarin <hoojunee@gmail.com>
2022-12-02 12:33:17 +0900

    markup_22.12.02

--- HTML/21.CSS(SVG)/jQuery.js
+++ HTML/21.CSS(SVG)/jQuery.js
@@ -1,5 +1,6 @@
 $(function () {
       var morph = new TimelineMax({paused:true}); 
-      morph.to(".path1",1,{morphSVG:".path2",fill:"#000",ease:Linear.easeNone})
-      .to(".path1", 1,{morphSVG:".path3",fill:"#000",ease:Linear.easeNone});
+
+      morph.to(".ex_four .path1",1,{morphSVG:".ex_four .path2",ease:Linear.easeNone})
+      .to(".ex_four .path2", 1,{morphSVG:".ex_four .path1_copy",ease:Linear.easeNone});
 })
