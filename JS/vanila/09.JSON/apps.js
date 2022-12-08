59c740a7bea57cdeab0e1041e21e73f196e7af4f
mandarin <hoojunee@gmail.com>
2022-12-08 16:57:45 +0900

    vanila_22.12.08

--- JS/vanila/09.JSON/apps.js
+++ JS/vanila/09.JSON/apps.js
@@ -1,11 +1,15 @@
 {/* ex_one */
     /*
     {
-        ~~~~ 일정들 ~~~~~
+      id : ,
+      date : ,
+      time : ,
+      todo : ,
     } 
     */
     /* 일정추가 ---------------------------------------------------- */
     // localStorage에서 일정을 확인한다. //
+
     let todoList
     let jsonLocalStorage = localStorage.getItem('localTodoList') // JSON화된 요소!(JSON.stringify)
     // '일정이 없다'라면 빈배열 //
@@ -18,15 +22,31 @@
     // 일정이 존재하면 일정목록을 배열로 저장 //
 
     document.querySelector('.ex_one button').addEventListener('click', () => {
+        let id = Date.now()
         let date = document.querySelector('.ex_one input[type=date]').value
         let time = document.querySelector('.ex_one input[type=time]').value
         let todo = document.querySelector('.ex_one input[type=text]').value
-        let todoObj = {date,time,todo,}//일정한개의 객체
+        let todoObj = {id,date,time,todo,}//일정한개의 객체
         todoList.push(todoObj)
         let jsonTodoList = JSON.stringify(todoList)//이 JS의 값을 JSON의 글자형태로 변환시켜준다.
         localStorage.setItem('localTodoList', jsonTodoList);
         /* date : date => 이런꼴로 많이해서 간소화시켜서 쓸수 있다! */
     })//addEventListener
-    /* 일정출력 ---------------------------------------------------- */
+    /* 일정출력(출력은 2번해줘야한다!) ---------------------------------------------------- */
+    let listOutput = () => {
+        if(!jsonLocalStorage) return false
+        todoList.forEach(e => {
+            let list = document.createElement('li')
+            document.querySelector('.ex_one ul').append(list)// jQuery에서 .append 역할!
+            list.innerHTML = `
+                날짜 : ${e.date}<br>
+                시간 : ${e.time}<br>
+                일정 : ${e.todo}<br>
+                <br>
+            `
+        })//forEach
+    }//listOutput
+    listOutput();
+
     /* 일정삭제 ---------------------------------------------------- */
 }
