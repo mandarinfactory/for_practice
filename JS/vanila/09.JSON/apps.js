e669613dc5dd38cf5b843909875ba8fa324254df
mandarin <hoojunee@gmail.com>
2022-12-08 15:44:14 +0900

    vanila_22.12.08

--- JS/vanila/09.JSON/apps.js
+++ JS/vanila/09.JSON/apps.js
@@ -1,5 +1,7 @@
 {/* ex_one */
     /* 일정추가 ---------------------------------------------------- */
+    let jsonLocalStorage = localStorage.getItem('todoList')
+    console.log(jsonLocalStorage);
     let todoList = [];//일정들의 배열
     document.querySelector('.ex_one button').addEventListener('click', () => {
         let date = document.querySelector('.ex_one input[type=date]').value
@@ -8,7 +10,7 @@
         let todoObj = {date,time,todo,}//일정한개의 객체
         todoList.push(todoObj)
         let jsonTodoList = JSON.stringify(todoList)//이 JS의 값을 JSON의 글자형태로 변환시켜준다.
-        localStorage.setItem(todoList, jsonTodoList);
+        localStorage.setItem('todoList', jsonTodoList);
         /* date : date => 이런꼴로 많이해서 간소화시켜서 쓸수 있다! */
     })//addEventListener
     /* 일정출력 ---------------------------------------------------- */
