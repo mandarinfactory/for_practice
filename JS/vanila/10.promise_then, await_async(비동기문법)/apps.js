f7ac4f72a1c19a5ceeb1ab18176de5af6288fdd6
mandarin <hoojunee@gmail.com>
2022-12-09 16:46:51 +0900

    vanila_22.12.09

--- JS/vanila/10.promise_then, await_async(비동기문법)/apps.js
+++ JS/vanila/10.promise_then, await_async(비동기문법)/apps.js
@@ -5,7 +5,7 @@ let makeWheat = () => {
             console.log('밀 만들기 끝!!')
             resolve('wheat')
         },Math.ceil(Math.random() * 3))//setTimeout
-    })//promise -> 필히 하나의 parameter가 필요하다!
+    })//promise -> 필히 하나의 parameter가 필요하다!(주로 resolve를 사용한다.)
 }//makeWheat-function
 /*            
 let makeWheat = () => {
@@ -29,17 +29,35 @@ let makeWheat = () => {
 -> 비동기 문으로 call-back 문법을 단순화 시키기 위해서 만들어졌다.
 */
 let makePowder = (ingredient) => {
-    console.log(`${ingredient}로 밀가루 만들기 시작`)
-    setTimeout(() => {
-        console.log('밀가루 만들기 끝!!')
-        return 'powder'
-    },Math.ceil(Math.random() * 3))//setTimeout
+    return new Promise((resolve) => {
+        console.log(`${ingredient}로 밀가루 만들기 시작`)
+        setTimeout(() => {
+            console.log('밀가루 만들기 끝!!')
+            resolve('powder')
+        },Math.ceil(Math.random() * 3))//setTimeout
+    })//promise
 }//makePowder-function
 
 let makeBread = (ingredient) => {
-    console.log(`${ingredient}로 빵 만들기 시작`)
-    setTimeout(() => {
-        console.log('빵 만들기 끝!!')
-        return 'bread'
-    },Math.ceil(Math.random() * 3))//setTimeout
+    return new Promise((resolve) => {
+        console.log(`${ingredient}로 빵 만들기 시작`)
+        setTimeout(() => {
+            console.log('빵 만들기 끝!!')
+            resolve('bread')
+        },Math.ceil(Math.random() * 3))//setTimeout
+    })//promise
 }//makeBread-function
+
+makeWheat().then((result) => {
+    return makePowder(result)
+})//makeWheat_then -> 꼭 return으로 끝내줘야 .then으로 연결할수 있다! 안하면 또 call-back 미로 써야함!
+.then((result) => {
+    return makeBread(result)
+})//makePower_then
+.then((result) => {
+    console.log(`${result}완성!!!!!!!!!!!!!`)
+})
+/*
+then -> promise로 만든 function은 then으로 연결할수 있다.
+result는 그냥 parameter이므로 임의의 값이다.
+*/
