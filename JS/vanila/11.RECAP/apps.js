28b7648c7b6645f2e6ebf1a57664610c3884a528
mandarin <hoojunee@gmail.com>
2022-12-12 14:52:33 +0900

    vanila_22.12.12

--- JS/vanila/11.RECAP/apps.js
+++
@@ -1,2 +0,0 @@
-/* jQuery로 연결하는 방법! */
-$('header').load('login.html')
---
+++ JS/vanila/11.fetch/apps.js
@@ -0,0 +1,6 @@
+/* jQuery로 연결하는 방법! 
+$('header').load('login.html') */
+/* http://127.0.0.1:5500/JS/vanila/11.RECAP/login.html -> 로 연결된다! */
+
+
+fetch('header.html');
