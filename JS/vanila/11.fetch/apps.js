cd2374738bf1eaf912953070b4da8addc03b46d8
mandarin <hoojunee@gmail.com>
2022-12-12 14:55:49 +0900

    vanila_22.12.12

--- JS/vanila/11.fetch/apps.js
+++ JS/vanila/11.fetch/apps.js
@@ -3,4 +3,6 @@ $('header').load('login.html') */
 /* http://127.0.0.1:5500/JS/vanila/11.RECAP/login.html -> 로 연결된다! */
 
 
-fetch('header.html');
+fetch('header.html').then(result => {
+    console.log(result);
+})//fetch.then -> 비동기로 된다! ('html'다 읽고 나서 확인한다.)
