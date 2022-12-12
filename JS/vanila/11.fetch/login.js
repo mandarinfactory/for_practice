c722e83ba0d0e2fa91c4a6d87a51729de4f5913f
mandarin <hoojunee@gmail.com>
2022-12-12 16:40:20 +0900

    vanila_22.12.12

--- JS/vanila/11.fetch/login.js
+++ JS/vanila/11.fetch/login.js
@@ -4,4 +4,14 @@ document.querySelector('.loginBtn').addEventListener('click', async e => {
     console.log(data);
     let id = document.querySelector('.userId').value
     let pw = document.querySelector('.userPw').value
+    data.forEach(v => {
+        if(v.id === id && v.pw === pw) {
+            user = {
+                id,
+                pw,
+                name : v.name // 얘는 따로 줄이지 않고 일일히 써줘야한다!
+            }//user_object
+        }//if-else
+    })//forEach
+    console.log(user.id, user.pw, user.name);
 })//addEventListener
