58e23a1089912dcb9e75748ce549bdba440d78d4
mandarin <hoojunee@gmail.com>
2022-12-12 15:46:26 +0900

    vanila_22.12.12

--- JS/vanila/11.fetch/apps.js
+++ JS/vanila/11.fetch/apps.js
@@ -1,22 +1,33 @@
 /* jQuery로 연결하는 방법! 
 $('header').load('login.html') */
 /* http://127.0.0.1:5500/JS/vanila/11.RECAP/login.html -> 로 연결된다! */
+window.handleLogin = false
 
 let includeText = () => {
-    return new Promise (resolve => {
-        fetch('header.html').then(result => {
+     /* fetch('header.html').then(result => {
             return result.text();
         }).then(result => {
             document.querySelector('header').innerHTML = result;
-            /* 로그인했으면, login_after를 보여주자! */
-            resolve();
-        })//fetch.then
-    })//promise
+            // 로그인했으면, login_after를 보여주자! //
+        })//fetch.then --> 다시 만들꺼임 뀽.... */
+
 }//includeText_function
 
+
 let loginHandler = async () => {
-    await includeText();
-}// ---> 로그인체크함수()!!!
+    let result = await fetch('header.html')
+    result = await result.text();
+    document.querySelector('header').innerHTML = result;
+    // 로그인했으면, login_after를 보여주자! //
+}//loginHandler_function ---> 로그인체크함수()!!!
+let loggedCheck = () => {
+    if(handleLogin) {
+        document.querySelector('.login_after').style.display = 'block'
+    } else {
+        document.querySelector('.login_before').style.display = 'block'
+    }//if-else
+}//loggedCheck_function
+loggedCheck();
 /* 
 0. await 하려면 new Promise가 먼저 있어야한다!
 1. 항상! await를 쓰려면 선 async가 있어야한다!
