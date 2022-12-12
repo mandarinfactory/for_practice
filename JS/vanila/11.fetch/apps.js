7a9174a8c2bd0d97a93b64a5e8a29e4644cf4f64
mandarin <hoojunee@gmail.com>
2022-12-12 15:05:47 +0900

    vanila_22.12.12

--- JS/vanila/11.fetch/apps.js
+++ JS/vanila/11.fetch/apps.js
@@ -4,5 +4,13 @@ $('header').load('login.html') */
 
 
 fetch('header.html').then(result => {
-    console.log(result.text);
-})//fetch.then -> 비동기로 된다! ('html'다 읽고 나서 확인한다.)
+    return result.text();
+}).then(result => {
+    document.querySelector('header').innerHTML = result
+})
+/* 
+1. fetch.then -> 비동기로 된다! ('html'다 읽고 나서 확인한다.) 
+2. 첫번째 result에는 완전 raw file로 다 가져온다. -> fetch('header.html').then(result => {})
+3. return result.text()로 text화 된걸 다시 비동기로 가져온다.
+4. html 전체를 긁어오게 된다.
+*/
