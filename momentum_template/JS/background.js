610cec14a7df4689e3f9d27ea28228c1394f2b95
mandarin <hoojunee@gmail.com>
2022-12-13 16:22:50 +0900

    react_22.12.13

--- momentum_template/JS/background.js
+++ momentum_template/JS/background.js
@@ -1,4 +1,4 @@
-const images = ["1.webp", "2.jpg", "3.jpg", "4.jpg", "5.png", "6.jpg", "7.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"];
 
 const randomImages = images[Math.floor(Math.random() * images.length)] 
 
--- react_template/src/App.js
+++ react_template/src/App.js
@@ -1,8 +1,12 @@
 const App = () => {
   return (
     <>
-      app
+      REACT 첫 수업 END!!!!!
     </>
+    /* 
+    HTML tag(<></>)를 적는다! --> 일단 먼저 청사진으로 여기다가 적는것! 
+    단, return안에는 하나의 element만 가능하다!
+    */
   );
 };
 export default App;
