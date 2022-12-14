47fc41f42fc990bc5affcf2f05c7a1c410e6226c
mandarin <hoojunee@gmail.com>
2022-12-14 17:12:55 +0900

    resume_22.12.14

--- react_template/src/App.js
+++ react_template/src/App.js
@@ -1,11 +1,12 @@
 const App = () => {
   return (
     <>
-      REACT 첫 수업 END!!!!!
+      <img htmlfor="이게 html에서for" className="이게 html에서class" defaultValue="이게 html에서value" src={`${process.env.PUBLIC_URL}/img/odung.png`} alt=""/>
     </>
     /* 
     HTML tag(<></>)를 적는다! --> 일단 먼저 청사진으로 여기다가 적는것! 
     단, return안에는 하나의 element만 가능하다!
+    {}는 JS를 코딩할때 꼭 써야한다!
     */
   );
 };
