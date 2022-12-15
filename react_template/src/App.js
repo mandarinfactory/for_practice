c93fe13754f3f50477ed9a04c7e8913c027577a8
mandarin <hoojunee@gmail.com>
2022-12-15 15:46:27 +0900

    react_22.12.15

--- react_template/src/App.js
+++ react_template/src/App.js
@@ -14,21 +14,34 @@ const App = () => {
  /* ----------------------------------------------------------------------- */
  const [stNum1, chStNum1] = useState(setNum())
  const [stNum2, chStNum2] = useState(setNum())
- const resultNum = () => {
-
+ const [stResultNum, chStResultNum] = useState('결과!!!')
+ const resultNum = event => {
+  event.preventDefault();
+  let userNum = parseInt(document.querySelector('.user_num').value)
+  if (stNum1 + stNum2 === userNum) {
+    chStResultNum('정답입니다!!!!!!ㅊㅊㅊ')
+  } else {
+    chStResultNum('오답입니다!!!!!!멍청아')
+  }//if-else
+  /* └-> 아래에서 이미 <form onSubmit~>을 썼기때문에 parameter가 아닌 event로써 
+  쓰이므로 preventDefault가 쓰일수 있다! */
  }
   /* ----------------------------------------------------------------------- */
   return (
     <form onSubmit={resultNum}>
-    {/* └-> 제대로 된 HTML element가 아니므로 querySelector 할수없어서 'on-'을 써야한다! */}
+    {/*
+   └-> 제대로 된 HTML element가 아니므로 querySelector 할수없어서 'on-'을 써야한다! 
+   'on-'을 쓰게되면, 해당함수 (여기서는 resultNum) 에서 parameter가 아닌 event이고(event-binding) 
+   쓰지 않는다면, 그 반대로 parameter로만 쓰인다.
+  */}
     <input type="text" readOnly defaultValue={stNum1} size="1" />
     +
     <input type="text" readOnly defaultValue={stNum2} size="1" />
     =
-    <input type="text" size="1" />
+    <input className="user_num" type="text" size="1" />
     <button style={{display : 'none'}}>결과보기!</button>
     {/* react에서 inline-style은 {{}}로 묶어서 {{display : 'none'}} 꼴로 적어야한다. */}
-    <p className="result">결과!</p>
+    <p className="result">{stResultNum}</p>
     </form>
     /* HTML공간! */
     );//return(JSX)
