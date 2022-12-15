d0110700ed1e40b94f3dc6bf4c67876a9955d33c
mandarin <hoojunee@gmail.com>
2022-12-15 15:50:26 +0900

    react_22.12.15

--- react_template/src/App.js
+++ react_template/src/App.js
@@ -18,13 +18,15 @@ const App = () => {
  const resultNum = event => {
   event.preventDefault();
   let userNum = parseInt(document.querySelector('.user_num').value)
-  if (stNum1 + stNum2 === userNum) {
-    chStResultNum('정답입니다!!!!!!ㅊㅊㅊ')
-  } else {
-    chStResultNum('오답입니다!!!!!!멍청아')
-  }//if-else
   /* └-> 아래에서 이미 <form onSubmit~>을 썼기때문에 parameter가 아닌 event로써 
   쓰이므로 preventDefault가 쓰일수 있다! */
+/*   if (stNum1 + stNum2 === userNum) {
+    chStResultNum('정답입니다!!!!!!ㅊㅊㅊ')
+  } else {
+    chStResultNum('오답입니다!!!!!!멍청아') 
+  }//if-else --> 이거 안쓰려고 react쓰는거임!!!!!!!! */
+  chStResultNum((stNum1 + stNum2 === userNum) ? '정답입니다!!!!!!ㅊㅊㅊ' : '오답입니다!!!!!!멍청아');
+  /* └-> react에서는 if문 보다는 3항 연산자를 더 많이 사용한다!!!!!!!!!! */
  }
   /* ----------------------------------------------------------------------- */
   return (
