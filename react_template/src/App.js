6afffb117a46bc4869e48ea61c91213923e50640
mandarin <hoojunee@gmail.com>
2022-12-16 15:14:41 +0900

    react_22.12.16

--- react_template/src/App.js
+++ react_template/src/App.js
@@ -5,16 +5,28 @@ import { useState } from "react";
 /* └-> 주석처리로 하면 warning은 따로 안나오고 error창만 뜨게 된다.  */
 const App = () => {
   /* ---------------------------------------------------------------- */
-  const [stNum1, chStNum1] = useState(setRandomNum())
-  const [stNum2, chStNum2] = useState(setRandomNum())
-  const [stResult, chStResult] = useState("")
-  const [stPoint, chStPoint] = useState("")
+  let [stNum1, chStNum1] = useState(setRandomNum())
+  let [stNum2, chStNum2] = useState(setRandomNum())
+  let [stResult, chStResult] = useState("")
+  let [stPoint, chStPoint] = useState("")
   /* ---------------------------------------------------------------- */
   function setRandomNum() {
-    return Math.floor(Math.random() * 10)
+    return Math.floor(Math.random() * 10);
   }//setRandomNum
   const checkResult = e => {
     e.preventDefault();
+    let userNum = parseInt(document.querySelector('input').value);
+    if (stNum1 + stNum2 === userNum) {
+      chStResult('정답');
+      chStPoint(++stPoint)
+      /* 
+      stPoint++로 출력하면 '0'출력된후 --> 한번더 클릭해야 '1'이 출력되므로 ++stPoint로 해야한다. 
+      또한, react에서는 가급적이면 증감연산자를 지양한다!!!!!!!
+      */
+    } else {
+      chStResult('오답');
+      chStPoint(--stPoint)
+    }//if-else
   }//checkResult
   /* ---------------------------------------------------------------- */
   return (
@@ -24,7 +36,7 @@ const App = () => {
         +
         <span>{stNum2}</span>
         =
-        <input type="text" size="1" />
+        <input className="" type="text" size="1" />
       </p>
       <hr />
       <p className="result">
