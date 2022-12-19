08aec9a7b0dcf726c0956498241cee85d81fb2e9
mandarin <hoojunee@gmail.com>
2022-12-19 15:59:49 +0900

    react_22.12.19

--- react_template/src/App.js
+++ react_template/src/App.js
@@ -4,8 +4,8 @@ import CompResult from "./CompResult";
 
 const App = () => {
   let [stShowComp, chStShowComp] = useState('calc')
-  const chComponent = () => {
-    chStShowComp('result')
+  const chComponent = showComp => {
+    chStShowComp(showComp)
   }//chComponent_function
   /* state갱신시점!!!!! ---------------------------------------------------------------------------------------- */
   return (
@@ -14,8 +14,9 @@ const App = () => {
         /* {(stShowComp === 'calc') && <CompClac />} --> true면 <CompClac />를 출력, false면 false를 출력한다. */
         /* {(stShowComp === 'calc') && <CompResult />} --> true면 <CompResult />를 출력, false면 false를 출력한다. */
       }
-      <button>문제화면</button>
-      <button onClick={chComponent}>결과화면</button>
+      <button onClick={() => { chComponent('calc') }}>문제화면</button>
+      <button onClick={() => { chComponent('result') }}>결과화면</button>
+      {/* 저번에도 써놨지만!!!! parameter를 쓰려면 () => {function (원하는 function('parameter'))} --> 꼴로 적어줘야한다. */}
       {(stShowComp === 'calc') && <CompClac />}
       {(stShowComp === 'result') && <CompResult />}
     </>
