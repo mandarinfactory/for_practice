be52e2850ddb7b474806af341d88ab174efd0b74
mandarin <hoojunee@gmail.com>
2022-12-19 15:10:03 +0900

    react_22.12.19

--- react_template/src/App.js
+++ react_template/src/App.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import CompClac from "./CompCalc";
 import CompResult from "./CompResult";
 
 const App = () => {
+  let [stShowComp, chStShowComp] = useState('calc')
   return (
     <>
-      <CompClac />
-      <CompResult />
+      { /* {(stShowComp === 'calc') ? <CompClac /> : <CompResult />} --> 3항식연산자 */
+        /* {(stShowComp === 'calc') && <CompClac />} --> true면 <CompClac />를 출력, false면 false를 출력한다. */
+        /* {(stShowComp === 'calc') && <CompResult />} --> true면 <CompResult />를 출력, false면 false를 출력한다. */
+      }
+      {(stShowComp === 'calc') && <CompClac />}
+      {(stShowComp === 'calc') && <CompResult />}
     </>
   );
 };
