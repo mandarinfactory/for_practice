ca874c0696222b06394b1c327acbeb9e76ecb71b
mandarin <hoojunee@gmail.com>
2022-12-19 15:26:04 +0900

    react_22.12.19

--- react_template/src/App.js
+++ react_template/src/App.js
@@ -4,6 +4,10 @@ import CompResult from "./CompResult";
 
 const App = () => {
   let [stShowComp, chStShowComp] = useState('calc')
+  const chComponent = () => {
+    chStShowComp('result')
+  }//chComponent_function
+  /* state갱신시점!!!!! ---------------------------------------------------------------------------------------- */
   return (
     <>
       { /* {(stShowComp === 'calc') ? <CompClac /> : <CompResult />} --> 3항식연산자 */
@@ -11,9 +15,9 @@ const App = () => {
         /* {(stShowComp === 'calc') && <CompResult />} --> true면 <CompResult />를 출력, false면 false를 출력한다. */
       }
       <button>문제화면</button>
-      <button>결과화면</button>
+      <button onClick={chComponent}>결과화면</button>
       {(stShowComp === 'calc') && <CompClac />}
-      {(stShowComp === 'calc') && <CompResult />}
+      {(stShowComp === 'result') && <CompResult />}
     </>
   );
 };
