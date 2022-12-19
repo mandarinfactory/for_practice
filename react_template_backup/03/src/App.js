9f94f84bd731bc137d84abf10728da2ccfd9f201
mandarin <hoojunee@gmail.com>
2022-12-19 14:41:06 +0900

    react_22.12.19

---
+++ react_template_backup/03/src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+/* └-> 주석처리로 하면 warning은 따로 안나오고 error창만 뜨게 된다.  */
+import { useState } from "react";
+import React, { Component } from 'react'
+import CompQuestion from "./component/CompQuestion";
+import CompResult from "./component/CompResult";
+
+const App = () => {
+  let [stShowComp, chStShowComp] = useState(1)
+  return (
+    <>
+      {
+        (stShowComp === 1) ? <CompQuestion chStShowComp={chStShowComp} /> : <CompResult />
+      }
+    </>
+  );
+};
+export default App;
