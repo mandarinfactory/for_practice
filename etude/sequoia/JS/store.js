630879b9e0cb7e335a21e0e629c7f0e5a4b494fd
mandarin <hoojunee@gmail.com>
2022-12-20 18:10:05 +0900

    sequoia_22.12.20

---
+++ etude/sequoia/JS/store.js
@@ -0,0 +1,175 @@
+window.calendarArr1 = [
+    {
+        date: '1',
+        minus: '-1,200',
+        plus: ''
+    },
+    {
+        date: '2',
+        minus: '-20,000',
+        plus: ''
+    },
+    {
+        date: '3',
+        minus: '-100,000',
+        plus: '+2'
+    },
+    {
+        date: '4',
+        minus: '',
+        plus: ''
+    },
+    {
+        date: '5',
+        minus: '-4,999',
+        plus: ''
+    },
+    {
+        date: '6',
+        minus: '-102,110',
+        plus: ''
+    },
+    {
+        date: '7',
+        minus: '',
+        plus: ''
+    },
+]
+
+
+window.calendarArr2 = [
+    {
+        date: '8',
+        minus: '-326,000',
+        plus: ''
+    },
+    {
+        date: '9',
+        minus: '',
+        plus: '+10,000'
+    },
+    {
+        date: '10',
+        minus: '-17,000',
+        plus: '+2,530,688'
+    },
+    {
+        date: '11',
+        minus: '-2,400',
+        plus: ''
+    },
+    {
+        date: '12',
+        minus: '',
+        plus: '+11,000'
+    },
+    {
+        date: '13',
+        minus: '-6,000',
+        plus: ''
+    },
+    {
+        date: '14',
+        minus: '-22,000',
+        plus: ''
+    },
+
+]
+
+
+window.calendarArr3 = [
+
+    {
+        date: '15',
+        minus: '-14,200',
+        plus: ''
+    },
+    {
+        date: '16',
+        minus: '-30,000',
+        plus: ''
+    },
+    {
+        date: '17',
+        minus: '',
+        plus: '+103,507'
+    },
+    {
+        date: '18',
+        minus: '',
+        plus: '+2'
+    },
+    {
+        date: '19',
+        minus: '-54,842',
+        plus: '+5,400'
+    },
+    {
+        date: '20',
+        minus: '-126,484',
+        plus: ''
+    },
+    {
+        date: '21',
+        minus: '-10,000',
+        plus: ''
+    },
+]
+
+
+window.calendarArr4 = [
+    {
+        date: '22',
+        minus: '-14,000',
+        plus: ''
+    },
+    {
+        date: '23',
+        minus: '-14,000',
+        plus: ''
+    },
+    {
+        date: '24',
+        minus: '',
+        plus: '+50,460'
+    },
+    {
+        date: '25',
+        minus: '-6,500',
+        plus: ''
+    },
+    {
+        date: '26',
+        minus: '-111,000',
+        plus: ''
+    },
+    {
+        date: '27',
+        minus: '-49,390',
+        plus: '+24,500'
+    },
+    {
+        date: '28',
+        minus: '',
+        plus: '+9,500'
+    },
+]
+
+
+window.calendarArr5 = [
+    {
+        date: '29',
+        minus: '-2,900',
+        plus: ''
+    },
+    {
+        date: '30',
+        minus: '',
+        plus: ''
+    },
+    {
+        date: '31',
+        minus: '-5,300',
+        plus: '+1,000'
+    },
+]
