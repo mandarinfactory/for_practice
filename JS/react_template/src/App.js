824417529b77f9ccf7228b6642b713e5a2844480
mandarin <hoojunee@gmail.com>
2022-12-21 15:00:21 +0900

    react_22.12.21

--- JS/react_template/src/App.js
+++ JS/react_template/src/App.js
@@ -1,7 +1,21 @@
 const App = () => {
   return (
     <>
-      app
+      <h1>더하기 빼기!</h1>
+      <hr />
+      <h2>더하기</h2>
+      <form>
+        <span></span>
+        +
+        <span></span>
+        =
+        <input type="text" size="1" />
+      </form>
+      <hr />
+      <h2>빼기</h2>
+      <form>
+      </form>
+      <p className="output">결과</p>
     </>
   );
 };
