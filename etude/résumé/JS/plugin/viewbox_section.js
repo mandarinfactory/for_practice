0376da122def1b0cdd820f015e965a9991d08f2f
mandarin <hoojunee@gmail.com>
2022-12-23 15:25:29 +0900

    résumé_22.12.23

--- etude/résumé/JS/plugin/viewbox_section.js
+++ etude/résumé/JS/plugin/viewbox_section.js
@@ -1,9 +1,9 @@
 $(function () {
+    /* viewbox_plugin_sunshine -------------------------------------- */
     $(window).load(function () {
         $('.viewbox_sunshine ul li figure a').viewbox();
     })//load
-    /* viewbox_plugin -------------------------------------- */
-    function callMotion() {
+    function callMotionSunshine() {
         $('.viewbox_sunshine ul li').each(function () {
             var offsetTop = $(this).offset().top
             if (scrY = offsetTop) {
@@ -13,11 +13,30 @@ $(function () {
             }
         })//each
     }//callMotion_function
-    callMotion();
+    callMotionSunshine();
     $(window).scroll(function () {
 
     }).resize(function () {
 
     })//window_event
-    /* scroll_motion ------------------- ------------------- */
+    /* viewbox_plugin_museum -------------------------------------- */
+    $(window).load(function () {
+        $('.viewbox_museum ul li figure a').viewbox();
+    })//load
+    function callMotionMuseum() {
+        $('.viewbox_museum ul li').each(function () {
+            var offsetTop = $(this).offset().top
+            if (scrY = offsetTop) {
+                $(this).addClass('active')
+            } else {
+                $(this).removeClass('active')
+            }
+        })//each
+    }//callMotion_function
+    callMotionMuseum();
+    $(window).scroll(function () {
+
+    }).resize(function () {
+
+    })//window_event
 })//document.ready
