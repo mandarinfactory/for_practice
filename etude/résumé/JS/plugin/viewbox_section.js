45f9f5623c08f573b956af56fddcbc1e00fdee43
mandarin <hoojunee@gmail.com>
2022-12-23 14:40:03 +0900

    résumé_22.12.23

--- etude/résumé/JS/plugin/viewbox_section.js
+++ etude/résumé/JS/plugin/viewbox_section.js
@@ -1,10 +1,10 @@
 $(function () {
     $(window).load(function () {
-        $('.viewbox_section ul li figure a').viewbox();
+        $('.viewbox_sunshine ul li figure a').viewbox();
     })//load
     /* viewbox_plugin -------------------------------------- */
-    function callMotion () {
-        $('.viewbox_section ul li').each(function () {
+    function callMotion() {
+        $('.viewbox_sunshine ul li').each(function () {
             var offsetTop = $(this).offset().top
             if (scrY = offsetTop) {
                 $(this).addClass('active')
@@ -14,9 +14,9 @@ $(function () {
         })//each
     }//callMotion_function
     callMotion();
-    $(window).scroll (function () {
+    $(window).scroll(function () {
 
-    }).resize (function () {
+    }).resize(function () {
 
     })//window_event
     /* scroll_motion ------------------- ------------------- */
