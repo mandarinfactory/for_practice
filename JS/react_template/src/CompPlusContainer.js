cd31ac60b4c72a57cde4e721b84e88029b75a9ce
mandarin <hoojunee@gmail.com>
2022-12-23 13:00:44 +0900

    react_22.12.23

--- JS/react_template/src/CompPlusContainer.js
+++ JS/react_template/src/CompPlusContainer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import CompPlus from './CompPlus';
 
+let plusArr = Array(3).fill(1)
 const CompPlusContainer = () => {
     return (
         <>
-            <CompPlus />
-            <CompPlus />
-            <CompPlus />
+            {
+                plusArr.map((v, i) => {
+                    return <CompPlus key={i} />
+                })//map
+            }
+
         </>
     );
 };
