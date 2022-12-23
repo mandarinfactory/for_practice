71785f6e9dfa843e5fbdb8ea72dadb9f42b4e052
mandarin <hoojunee@gmail.com>
2022-12-23 10:43:55 +0900

    react_22.12.23

--- JS/react_template/src/App.js
+++ JS/react_template/src/App.js
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
-import CompMinus from "./CompMinus";
-import CompPoint from "./CompPoint";
-import CompSum from "./CompSum";
-import CompTitle from "./CompTitle";
-
 const App = () => {
-  let [point, chPoint] = useState(0)
-  useEffect(() => {
-    console.log('AppComponent 완성!!!!!!');
-  }, [])//useEffect
-  useEffect(() => {
-    console.log('AppComponent 수정!!!!!!');
-  }, [])//useEffect
   return (
     <>
-      <CompTitle />
-      <hr />
-      <CompSum point={point} chPoint={chPoint} />
-      <hr />
-      <CompMinus point={point} chPoint={chPoint} />
-      <hr />
-      <CompPoint point={point} />
+      app
     </>
   );
 };
