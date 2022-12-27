d7ebad5d5b2aad23a04f3c2be9f1a65b4d5ac3cb
mandarin <hoojunee@gmail.com>
2022-12-27 12:50:58 +0900

    react_22.12.27
    making calculator reseting
    add image and CSS and JS files

--- JS/react_template/src/App.js
+++ JS/react_template/src/App.js
@@ -1,26 +1,8 @@
-import { createContext, useContext } from "react";
-import { Route, Routes } from "react-router-dom";
-import Comp404 from "./Comp404";
-import CompMinusContainer from "./CompMinusContainer";
-import CompNav from "./CompNav";
-import CompPlusContainer from "./CompPlusContainer";
-export let myContext = createContext();
-/* 'myContext'는 export <--> import를 해야하는데 이미 App이 export되어있는곳이 아닌 밖에서 선언해야한다!!! */
-
-let setNumber = () => {
-  return Math.floor(Math.random() * 9)
-}//function_setNumber
-
 const App = () => {
   return (
-    <myContext.Provider value={setNumber}>
-      <CompNav />
-      <Routes>
-        <Route path="/" element={<CompPlusContainer />} />
-        <Route path="minus" element={<CompMinusContainer />} />
-        <Route path="*" element={<Comp404 />} />
-      </Routes>
-    </myContext.Provider>
+    <>
+      app
+    </>
   );
 };
 export default App;
