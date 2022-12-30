8343db4dab00a6dfc7613fc8a132bb41a63cc304
mandarin <hoojunee@gmail.com>
2022-12-30 13:00:57 +0900

    react_22.12.30
    계산기만들기
    전체적인 계산기 완성후, error 페이지 만듦 + click하면 error 사라지게 만듦

--- JS/calculator/src/Comp/CompError.js
+++ JS/calculator/src/Comp/CompError.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
 
 const CompError = () => {
+    let { chShowErr } = useContext(AppContext)
+    let hideErr = e => {
+        e.currentTarget.classList.add('active')
+        e.currentTarget.addEventListener('transitionend', () => {
+            chShowErr(false)
+        })//addEventListener
+    }//function_hideErr
     return (
-        <figure className='error'>
+        <figure className='error' onClick={hideErr}>
             <img src={require('../Assets/img/oops.png')} alt />
         </figure>
     );
