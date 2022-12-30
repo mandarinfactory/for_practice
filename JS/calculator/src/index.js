3f79bdecc9ef094261cd5d4006a07fc42228d0f6
mandarin <hoojunee@gmail.com>
2022-12-30 16:13:01 +0900

    react_22.12.30
    calculator migration --> npm run build를 통해서 build만들고 manifest 수정함

--- JS/calculator/src/index.js
+++ JS/calculator/src/index.js
@@ -10,5 +10,18 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register(() => {
+  window.addEventListener('activate', function (event) {
+    event.waitUntil(
+      caches.keys().then(function (cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function (cacheName) {
+          }).map(function (cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
+});
 
