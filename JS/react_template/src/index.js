7abadc114cc3da07937f455ff72cbb54c33fe0b1
mandarin <hoojunee@gmail.com>
2023-01-02 15:17:57 +0900

    react_23.01.02
    react webpage 기본 설정(img, font연결 등) + font 연결확인

--- JS/react_template/src/index.js
+++ JS/react_template/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './asset/CSS/reset.css'
+import './asset/CSS/style.css'
+import './asset/CSS/screen/header.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
   </>,
-);
+); 
