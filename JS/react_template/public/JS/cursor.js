b9e845a643535bb99de080a49c9c67b923301aeb
mandarin <hoojunee@gmail.com>
2023-01-02 17:17:27 +0900

    react_23.01.02
    react webpage 기본 설정mouse cursor custom으로 제작 및 animation 설정

--- JS/react_template/public/JS/cursor.js
+++ JS/react_template/public/JS/cursor.js
@@ -2,6 +2,7 @@ window.addEventListener('mousemove', () => {
     document.querySelector('.cursor').style.left = `${window.mausX}px`
     document.querySelector('.cursor').style.top = `${window.mausY}px`
 })//mousemove_event
+
 document.querySelectorAll('a,button').forEach(v => {
     v.addEventListener('mouseenter', () => {
         document.querySelector('.cursor').classList.add('hover')
@@ -9,4 +10,12 @@ document.querySelectorAll('a,button').forEach(v => {
     v.addEventListener('mouseleave', () => {
         document.querySelector('.cursor').classList.remove('hover')
     })//addEventListener
+    /* mouseenter/leave --------------------------------------------------- */
+    v.addEventListener('mouseup', () => {
+        document.querySelector('.cursor').classList.remove('active')
+    })//addEventListener
+    v.addEventListener('mousedown', () => {
+        document.querySelector('.cursor').classList.add('active')
+    })//addEventListener
+    /* mouseup/down ------------------------------------------------------- */
 })//forEach
