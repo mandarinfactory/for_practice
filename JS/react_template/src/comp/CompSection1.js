76bf8cdbb415a093dca1a2e18d4f715b2839fb31
mandarin <hoojunee@gmail.com>
2023-01-12 16:09:20 +0900

    react_23.01.12
    section2 video, para. animation 조정

--- JS/react_template/src/comp/CompSection1.js
+++ JS/react_template/src/comp/CompSection1.js
@@ -4,6 +4,7 @@ const CompSection1 = () => {
   return (
     <section className='section1'>
       <video className='bg' src={require('../asset/video/video.mp4')} loop autoPlay muted playsInline />
+      <video className='light' src={require('../asset/video/light.mp4')} loop autoPlay muted playsInline />
       <div className="wrap">
         <div className="videos">
           <video className='video1' src={require('../asset/video/video.mp4')} loop autoPlay muted playsInline />
@@ -13,7 +14,7 @@ const CompSection1 = () => {
         <div className="textbox">
           <small>Dance Classes</small>
           <h2>
-            <span>Modern</span>
+            <span>Modern </span>
             <span>&amp; Classic</span>
           </h2>
           <p>
@@ -21,7 +22,10 @@ const CompSection1 = () => {
               코딩없이 시각적으로 추가, 편집, 삭제 그리고 수정이 가능하다.
             </em>
           </p>
-          <a href="#">learn more</a>
+          <a href="#">
+            learn more
+            <i className="fa-solid fa-arrow-right"></i>
+          </a>
         </div>{/* textbox */}
       </div>{/* wrap */}
     </section >
