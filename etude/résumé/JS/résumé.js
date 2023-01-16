ecd2a137e51e02d6e01ef8d33af7ba0ebcebf690
mandarin <hoojunee@gmail.com>
2023-01-16 09:50:40 +0900

    이력서_23.01.16
    주말에 만들어놓은거 all pull

--- etude/résumé/JS/résumé.js
+++ etude/résumé/JS/résumé.js
@@ -1,3 +1,17 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray("section").forEach((section, i) => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: "top top", 
+    pin: true,
+    pinSpacing: false,
+  })//create
+})//forEach
+ScrollTrigger.create({
+  snap: 1 / 5000
+})//create
+/* folder_scroll_animation 작동법 ---------------------------------------------------------------------- */
+
 const button0 = document.querySelector('.menu_box .button0')
 const button1 = document.querySelector('.menu_box .button1')
 const button2 = document.querySelector('.menu_box .button2')
@@ -9,6 +23,13 @@ const barBtn = document.querySelector('.mbtn .bars')
 const closeBtn = document.querySelector('.mbtn .close')
 const menuBox = document.querySelector('.menu_box')
 const rightArrow = document.querySelector('.arrow_right')
+const section0 = document.querySelector('.section01')
+const section1 = document.querySelector('.section02')
+const section2 = document.querySelector('.section03')
+const section3 = document.querySelector('.section04')
+const section4 = document.querySelector('.section05')
+const section5 = document.querySelector('.section06')
+const section6 = document.querySelector('.section07')
 const menuBtn = Array.from(document.querySelectorAll('.menu_box button'))
 const handleClick = e => {
     menuBtn.forEach(v => {
@@ -34,42 +55,42 @@ menuBtn.forEach(e => {
     e.addEventListener('click', handleClick)
 })//forEach
 
-document.addEventListener('scroll', e => {
+document.addEventListener('scroll', () => {
     document.querySelector('.arrow_down').style.opacity = 0
 })
 /* menu_box내에서 icon들 누르면 사라지게 하거나 button 누르면 색변경 시키게 해놓음 ---------------------- */
 
-button0.addEventListener('click', e => {
-    document.querySelector('.section_one').scrollIntoView({ behavior: 'smooth', block: 'center' })
+button0.addEventListener('click', () => {
+    section0.scrollIntoView({ behavior: 'smooth', block: 'start' })
 })//addEventListener
-button1.addEventListener('click', e => {
-    document.querySelector('.section_two').scrollIntoView({ behavior: 'smooth', block: 'center' })
+button1.addEventListener('click', () => {
+    section1.scrollIntoView({ behavior: 'smooth', block: 'start' })
 })//addEventListener
-button2.addEventListener('click', e => {
-    document.querySelector('.section_three').scrollIntoView({ behavior: 'smooth', block: 'center' })
+button2.addEventListener('click', () => {
+    section2.scrollIntoView({ behavior: 'smooth', block: 'start' })
 })//addEventListener
-button3.addEventListener('click', e => {
-    document.querySelector('.section_four').scrollIntoView({ behavior: 'smooth', block: 'center' })
+button3.addEventListener('click', () => {
+    section3.scrollIntoView({ behavior: 'smooth', block: 'start' })
 })//addEventListener
-button4.addEventListener('click', e => {
-    document.querySelector('.section_five').scrollIntoView({ behavior: 'smooth', block: 'center' })
+button4.addEventListener('click', () => {
+    section4.scrollIntoView({ behavior: 'smooth', block: 'start' })
 })//addEventListener
-button5.addEventListener('click', e => {
-    document.querySelector('.section_six').scrollIntoView({ behavior: 'smooth', block: 'center' })
+button5.addEventListener('click', () => {
+    section5.scrollIntoView({ behavior: 'smooth', block: 'start' })
 })//addEventListener
-button6.addEventListener('click', e => {
-    document.querySelector('.section_seven').scrollIntoView({ behavior: 'smooth', block: 'center' })
-})//addEventListener
+button6.addEventListener('click', () => {
+    section6.scrollIntoView({ behavior: 'smooth', block: 'start' })
+})//addEventListener */
 /* 해당 button 누르면 해당 section으로 가게 만들어줌 ---------------------------------------------------- */
 
 let scrollSpy = () => {
     let current
-    const offset1 = document.querySelector('.section_two').getBoundingClientRect().y + (scrY * 0.9)
-    const offset2 = document.querySelector('.section_three').getBoundingClientRect().y + (scrY * 0.9)
-    const offset3 = document.querySelector('.section_four').getBoundingClientRect().y + (scrY * 0.9)
-    const offset4 = document.querySelector('.section_five').getBoundingClientRect().y + (scrY * 0.9)
-    const offset5 = document.querySelector('.section_six').getBoundingClientRect().y + (scrY * 0.9)
-    const offset6 = document.querySelector('.section_seven').getBoundingClientRect().y + (scrY * 0.9)
+    const offset1 = section1.getBoundingClientRect().y + (scrY)
+    const offset2 = section2.getBoundingClientRect().y + (scrY)
+    const offset3 = section3.getBoundingClientRect().y + (scrY)
+    const offset4 = section4.getBoundingClientRect().y + (scrY)
+    const offset5 = section5.getBoundingClientRect().y + (scrY)
+    const offset6 = section6.getBoundingClientRect().y + (scrY)
     if (scrY < offset1) {
         current = 0
     } else if (scrY >= offset1 && scrY < offset2) {
@@ -85,7 +106,7 @@ let scrollSpy = () => {
     } else {
         current = 6
     }//if-else
-    document.querySelectorAll('.menu_box button').forEach(v => {
+    menuBtn.forEach(v => {
         v.classList.remove('active')
     })//forEach
     document.querySelector(`.menu_box .link_btn${current}`).classList.add('active')
@@ -109,16 +130,15 @@ let scrollObserver = new IntersectionObserver(e => {
     })//forEach
 })//function_scrollObserver
 
-const sections = document.querySelectorAll('.sections')
+const sections = document.querySelectorAll('.section')
 scrollObserver.observe(sections[0])/* 해당 HTML요소가 화면에 등장하는지 감시한다. --> observe */
 scrollObserver.observe(sections[1])
 scrollObserver.observe(sections[2])
 scrollObserver.observe(sections[3])
 scrollObserver.observe(sections[4])
 scrollObserver.observe(sections[5])
-
+scrollObserver.observe(sections[6])
 /* section으로 넘어갈때마다 확인해서 opacity줌 -------------------------------------------------------- */
-
 ScrollReveal({
     reset: false,
     distance: '70px',
@@ -126,14 +146,15 @@ ScrollReveal({
     delay: 500
 });
 
-ScrollReveal().reveal('.right ul li:first-child', { delay: 100, origin: 'right' });
-ScrollReveal().reveal('.right ul li:nth-child(2)', { delay: 100, origin: 'right' });
-ScrollReveal().reveal('.right ul li:nth-child(3)', { delay: 100, origin: 'right' });
-ScrollReveal().reveal('.right ul li:nth-child(4)', { delay: 100, origin: 'right' });
-ScrollReveal().reveal('.right ul li:nth-child(5)', { delay: 100, origin: 'right' });
-ScrollReveal().reveal('.right ul li.box', { delay: 100, origin: 'right' });
-ScrollReveal().reveal('.right ul li.address', { delay: 100, origin: 'right' });
-ScrollReveal().reveal('.right .arrow_right span', { delay: 100, origin: 'left', interval: 1000 });
-ScrollReveal().reveal('.right .arrow_right i', { delay: 100, origin: 'right', interval: 1000 });
-ScrollReveal().reveal('.right .arrow_down span', { delay: 100, origin: 'right', interval: 1000 });
-ScrollReveal().reveal('.right .arrow_down i', { delay: 100, origin: 'bottom', interval: 1000 });
+ScrollReveal().reveal('.right_content .arrow_right span', { delay: 100, origin: 'left', interval: 1000 });
+ScrollReveal().reveal('.right_content .arrow_right i', { delay: 100, origin: 'right', interval: 1000 });
+ScrollReveal().reveal('.right_content .arrow_down span', { delay: 100, origin: 'right', interval: 1000 });
+ScrollReveal().reveal('.right_content .arrow_down i', { delay: 100, origin: 'bottom', interval: 1000 });
+ScrollReveal().reveal('.right_content ul li:first-child', { delay: 100, origin: 'right' });
+ScrollReveal().reveal('.right_content ul li:nth-child(2)', { delay: 100, origin: 'right' });
+ScrollReveal().reveal('.right_content ul li:nth-child(3)', { delay: 100, origin: 'right' });
+ScrollReveal().reveal('.right_content ul li:nth-child(4)', { delay: 100, origin: 'right' });
+ScrollReveal().reveal('.right_content ul li:nth-child(5)', { delay: 100, origin: 'right' });
+ScrollReveal().reveal('.right_content ul li.box', { delay: 100, origin: 'right' });
+ScrollReveal().reveal('.right_content ul li.address', { delay: 100, origin: 'right' });
+
