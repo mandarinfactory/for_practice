c2496744445b95a13dec2f892aafd16a2825784d
mandarin <hoojunee@gmail.com>
2023-01-17 14:35:59 +0900

    react_23.01.17
    section3, sticky 부분 img, animation(parallax)추가함

--- JS/react_template/src/comp/CompSection3.js
+++ JS/react_template/src/comp/CompSection3.js
@@ -16,6 +16,60 @@ const CompSection3 = () => {
                   </figcaption>
                 </div>
               </figure>
+              <figure className='figure02'>
+                <div className='inner'>
+                  <img src={require('../asset/img/section3/section3-2.jpg')} alt="" />
+                  <figcaption>
+                    <em>Modern Dance</em>
+                    <small>Intermediate Age 10+</small>
+                  </figcaption>
+                </div>
+              </figure>
+              <figure className='figure03'>
+                <div className='inner'>
+                  <img src={require('../asset/img/section3/section3-3.jpg')} alt="" />
+                  <figcaption>
+                    <em>Lively Dance</em>
+                    <small>Intermediate Age 15+</small>
+                  </figcaption>
+                </div>
+              </figure>
+              <figure className='figure04'>
+                <div className='inner'>
+                  <img src={require('../asset/img/section3/section3-4.jpg')} alt="" />
+                  <figcaption>
+                    <em>Active Dance</em>
+                    <small>Intermediate Age 15+</small>
+                  </figcaption>
+                </div>
+              </figure>
+              <figure className='figure05'>
+                <div className='inner'>
+                  <img src={require('../asset/img/section3/section3-5.jpg')} alt="" />
+                  <figcaption>
+                    <em>Happiness Dance</em>
+                    <small>Intermediate Age 12+</small>
+                  </figcaption>
+                </div>
+              </figure>
+              <figure className='figure06'>
+                <div className='inner'>
+                  <img src={require('../asset/img/section3/section3-6.jpg')} alt="" />
+                  <figcaption>
+                    <em>Splendid Dance</em>
+                    <small>Intermediate Age 18+</small>
+                  </figcaption>
+                </div>
+              </figure>
+              <figure className='figure07'>
+                <div className='inner'>
+                  <img src={require('../asset/img/section3/section3-7.jpg')} alt="" />
+                  <figcaption>
+                    <em>Contemporary Dance</em>
+                    <small>Intermediate Age 18+</small>
+                  </figcaption>
+                </div>
+              </figure>
             </div>
           </div>
         </div>
