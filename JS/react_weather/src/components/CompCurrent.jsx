1101aa16ec4a95105a9dbe5ce3144205e8242267
mandarin <hoojunee@gmail.com>
2023-01-27 13:15:11 +0900

    reactWeather_23.01.27
    구글맵, 날씨 API 연동시킴

--- JS/react_weather/src/components/CompCurrent.jsx
+++ JS/react_weather/src/components/CompCurrent.jsx
@@ -4,11 +4,14 @@ import { Appcontext } from '../App';
 const CompCurrent = () => {
     let { address, weather } = useContext(Appcontext)
     if (!address || !weather) return false
+    let weatherData = weather.current // 현재 날씨 정보객체
     return (
         <section className='current_section'>
             <h1>날씨앱</h1>
             <p className="address">
-                {address[2].formatted_address}
+                {
+                    (address) ? address[2].formatted_address : '등록되지 않은 위치입니다.'
+                }
             </p>
         </section>
     );
