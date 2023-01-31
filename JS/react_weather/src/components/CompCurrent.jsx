6f826277ddbc2dc6d0bd5c2d3b5f18760fd77766
mandarin <hoojunee@gmail.com>
2023-01-31 11:21:10 +0900

    reactWeather_23.01.27
    사진변경

--- JS/react_weather/src/components/CompCurrent.jsx
+++ JS/react_weather/src/components/CompCurrent.jsx
@@ -13,64 +13,67 @@ const CompCurrent = () => {
     */
     return (
         <section className='current_section'>
+            <div className="circle"></div>
             <p className="address">
                 {
                     (address) ? address[2].formatted_address : '등록되지 않은 위치입니다.'
                 }
             </p>
             <div className="current_info">
-                <div className='sunrise_sunset'>
-                    <time>
-                        {/* {todayData.sunriseHour} */}:{/* {todayData.sunriseMin} */}
-                    </time>
-                    <time>
-                        {/* {todayData.sunsetHour} */}:{/* {todayData.sunsetMin} */}
-                    </time>
-                </div>
-                <p className="temp_min_max">
-                    {/*
-                     <b>{todayData.tempMin}</b> 
-                     <b>{todayData.tempMax}</b> 
-                     */}
-                </p>
-                <p className='temp'>
-                    <b>{weatherData.temp - 273.15}</b><sup>&#8451;</sup>
-                </p>
-                <p className="desc">
-                    {/* data.desc */}
-                </p>
-                <p className="wind_rain">
-                    {/* data.windDeg */},
-                    <i style={{ transform: `rotate(-45deg)` }}></i>
-                    <b>
-                        {/* data.windSpeed */}<small>hPa</small>
-                    </b>
-                    <i></i>
-                    <b>
-                        {/* data.rain */}<small>mm/h</small>
-                    </b>
-                </p>
-                <p className='time'>
-                    <time>
+                <div className="wrap">
+                    <div className='sunrise_sunset'>
+                        <time>
+                            {/* {todayData.sunriseHour} */}:{/* {todayData.sunriseMin} */}
+                        </time>
+                        <time>
+                            {/* {todayData.sunsetHour} */}:{/* {todayData.sunsetMin} */}
+                        </time>
+                    </div>
+                    <p className="temp_min_max">
+                        {/*
+                        <b>{todayData.tempMin}</b> 
+                        <b>{todayData.tempMax}</b> 
+                        */}
+                    </p>
+                    <p className='temp'>
+                        <b>{weatherData.temp - 273.15}</b><sup>&#8451;</sup>
+                    </p>
+                    <p className="desc">
+                        {/* data.desc */}
+                    </p>
+                    <p className="wind_rain">
+                        {/* data.windDeg */},
+                        <i style={{ transform: `rotate(-45deg)` }}></i>
                         <b>
-                            {
-                                /* {(data.hour <= 12) ? data.hour : data.hour - 12} : {data.minute}*/
-                            }
+                            {/* data.windSpeed */}<small>hPa</small>
                         </b>
-                        <small>
-                            ({((/* data.hour */ 12 <= 12)) ? 'AM' : 'PM'})
-                        </small>
-                    </time>
-                    <time>
-                        {/* <b>
-                                {data.year}년 {data.month}월 {data.date}일 
+                        <i></i>
+                        <b>
+                            {/* data.rain */}<small>mm/h</small>
+                        </b>
+                    </p>
+                    <p className='time'>
+                        <time>
+                            <b>
+                                {
+                                    /* {(data.hour <= 12) ? data.hour : data.hour - 12} : {data.minute}*/
+                                }
                             </b>
                             <small>
-                                {data.day} 
+                                ({((/* data.hour */ 12 <= 12)) ? 'AM' : 'PM'})
                             </small>
-                        */}
-                    </time>
-                </p>
+                        </time>
+                        <time>
+                            {/* <b>
+                                    {data.year}년 {data.month}월 {data.date}일 
+                                    </b>
+                                    <small>
+                                    {data.day} 
+                                    </small>
+                                */}
+                        </time>
+                    </p>
+                </div>
             </div>
         </section>
     );
